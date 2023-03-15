2bb9cc786b62289cbcbc9146c1b20152647ed1b1
tr1ang1e <tr1ang1e.goog1e@gmail.com>
2023-03-15 10:47:01 +0300

    better project hierarchy

--- bnc/bnc.py
+++ bnc/bnc.py
@@ -1,5 +1,8 @@
 import os
 import json
+import logging
+import logging.config
+from logging.handlers import RotatingFileHandler
 from time import sleep
 from binance.client import Client
 from binance import ThreadedWebsocketManager as WebSock
@@ -18,10 +21,19 @@ def get_credentials(credentials_file: str):
 def handle_price_msg(msg):
     """
         main business logic function
-        TODO: Implement two thread logic
+        TODO: implement two thread logic
     """
     print(msg["c"])   # "c" = last price
     return
+
+
+class Settings:
+    """
+        TODO:
+            parse settings file
+            parse credentials file
+    """
+    pass
 
 
 class Price:
@@ -67,6 +79,26 @@ class Account:
         self.api_demo = credentials_api["demo"]
         self.client = Client(self.api_demo["key_api"], self.api_demo["key_secret"])
 
+    def init_logging(self):
+        base_format = f"%(asctime)s%(msecs)03d [%(levelname)s] [%(threadName)-9s] %(message)s"
+        timestamp_format = "%H:%M:%S"
+        log_file = os.path.join(f"{os.path.abspath('.')}")
+        '''
+        log_file_handler = RotatingFileHandler(log_file, maxBytes=10 * 1024 * 1024, backupCount=2)
+        log_file_handler.setLevel(logging.DEBUG)
+        log_file_handler.setFormatter(logging.Formatter(SYS_LOG_FORMAT, SYS_LOG_TIMESTAMP_FORMAT))
+
+        log_console_handler = logging.StreamHandler()
+        log_console_handler.setLevel(logging.DEBUG)
+        log_console_handler.setFormatter(logging.Formatter(SYS_LOG_FORMAT, SYS_LOG_TIMESTAMP_FORMAT))
+
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(log_file_handler)
+        logger.addHandler(log_console_handler)
+        logger.info(f"{__file__} started")
+        '''
+
     def get_balance(self, assets=("btc", "usdt"), base_asset="usdt"):
         """
             Slow operation. Bad to call in WebSocket callback function.
@@ -97,8 +129,24 @@ class Account:
         price_list = self.client.get_symbol_ticker(symbol=symbol)
         return price_list['price']
 
-    def set_sell_limit_order(self, symbol, price, quantity):
-        print(self.client.order_limit_sell(symbol=symbol, price=price, quantity=quantity))
+    def set_buy_limit_order(self, symbol, buy_price, quantity):
+        """
+            function implements OPEN position logic (long-trading)
+            TODO:
+                - error handling
+                - return value
+        """
+        print(self.client.order_limit_buy(symbol=symbol, price=buy_price, quantity=quantity))
+        return
+
+    def set_sell_limit_order(self, symbol, sell_price, quantity):
+        """
+            function implements CLOSE position logic (long-trading)
+            TODO:
+                - error handling
+                - return value
+        """
+        print(self.client.order_limit_sell(symbol=symbol, price=sell_price, quantity=quantity))
         return
 
 
@@ -106,7 +154,7 @@ if __name__ == "__main__":
     btc = None
     price = None
     try:
-        account = Account("./credentials.json")
+        account = Account("./settings/credentials.json")
         price = Price()
         price.start_ticker("BTCUSDT", handle_price_msg)
         sleep(4)

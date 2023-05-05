34379f9558c21c31c74135a2f775b6f717db4427
tr1ang1e <tr1ang1e.goog1e@gmail.com>
2023-05-05 13:29:13 +0300

    master clean up

--- bnc/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .account import Account, Order
-from .exceptions import bnc_lib_exc_str, BNCAttention, BNCCritical, BNCExceptions
-from .price import Price
-from .settings import Api, Logfile, Settings
-from .trader import Eleven
-from .utilities import carriage_return, parse_arguments, init_logger
--- bnc/modules/tester.py
+++
@@ -1,6 +0,0 @@
-"""
-TODO:
-    - get historical data for period
-    - specify settings
-    - set callback which should be tested
-"""
